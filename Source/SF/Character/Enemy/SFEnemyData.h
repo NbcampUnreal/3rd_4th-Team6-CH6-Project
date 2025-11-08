20e909637a6a9aa656238b366b7d0e25a8f5e1fd
Parkjunbeom99 <pjb990@gmail.com>
2025-11-08 16:47:53 +0900

    [Add]Controller enemy, data생성

---
+++ Source/SF/Character/Enemy/SFEnemyData.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/SFPawnData.h"
+#include "SFEnemyData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SF_API USFEnemyData : public USFPawnData
+{
+	GENERATED_BODY()
+};
